b852701a037cade2fa229d6969384b578106a049
popstar123 <wwu3@saic.edu>
2018-08-15 21:01:02 -0500

    Added

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.use(
 
 let Contact = require("./models/code.js");
 
-app.post("/", function(req, res) {
+console.log("what happened");
+
+app.post("/submit", function(req, res) {
   Contact.create(req.body)
     // .then(res.json("Signed In"))
     .catch(err => res.status(422).json(err));
@@ -38,11 +40,27 @@ app.post("/", function(req, res) {
 // if (process.env.NODE_ENV === "production") {}
 // app.use(routes);
 
+// let databaseURL = "mongodb://localhost/winnieportfolio";
+
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect(databaseURL);
+// }
+
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/winnieportfolio"
+  process.env.MONGODB_URI || "mongodb://localhost/winnieportfolio2"
 );
 
-let db = mongoose.connection;
+// let db = mongoose.connection;
+
+// db.on("error", function(err) {
+//   console.log("Mongoose Error: ", err);
+// });
+
+// db.once("open", function() {
+//   console.log("Mongoose connection is successful");
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
